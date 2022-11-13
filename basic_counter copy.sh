00c8545d3b4f7446855caa1d279e32f776a467f6
Alex G <a.grigoriev@ze.studio>
2022-11-13 17:09:22 +0100

    loop and counter

---
+++ basic_counter copy.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+# basic while loop
+
+counter = 1
+
+while [$counter -le 10 ]
+do 
+echo $counter
+ ((counter++))
+ done
+
+ echo All done
---
+++ for_loop_series.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for value in {1..5}
+do
+echo $value
+done
+
+echo All done
